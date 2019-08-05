a6dbf36de08b6b2096325bf95a0717b0f8d10234
Kim van Wyk <kimv@sahomeloans.com>
2019-08-05 22:22:01 +0200

    Beginning reg form

---
+++ raw/registration/build_clubs_selector.py
@@ -0,0 +1,17 @@
+import pyperclip
+
+import sys
+
+def build_selector(fn):
+    out = []
+    with open(fn, "r") as fh:
+        for l in fh:
+            item = l.strip()
+            out.append(f'<option value="{item}">{item}</option>')
+    pyperclip.copy("\n".join(out))
+
+if sys.argv[1] == 'c':
+    build_selector("clubs.txt")
+if sys.argv[1] == 't':
+    build_selector("titles.txt")
+
